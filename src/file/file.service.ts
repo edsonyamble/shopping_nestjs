05e1b8ad4e2d588d56292fac977e40681d7854de
Edson Yamble <edsonyamble2@gmail.com>
2024-08-18 06:59:36 +0700

    file upload module controller service

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { path } from 'app-root-path';
+import { ensureDir, writeFile } from 'fs-extra';
+import { FileResponse } from './file.interface';
 
 @Injectable()
-export class FileService {}
+export class FileService {
+    async saveFile(file: Express.Multer.File[],folder:string='products') {
+       const uploadFolder = `${path}/uploads/${folder}`
+       await ensureDir(uploadFolder)//если нет папки то создаем
+       const response:FileResponse[]=await Promise.all(file.map(async file=>{
+           const originalName = `${Date.now()}-${file.originalname}`
+
+           await writeFile(`${uploadFolder}/${originalName}`,file.buffer)
+           return {name:originalName,url:`${uploadFolder}/${originalName}`}
+       }))
+
+       return response
+    }
+  
+}
+
