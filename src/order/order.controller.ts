0327c774d5d14657313f978e2de47611c7cb544c
Edson Yamble <edsonyamble2@gmail.com>
2024-08-19 06:42:34 +0700

    order dto module controller service

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,7 +1,33 @@
-import { Controller } from '@nestjs/common';
-import { OrderService } from './order.service';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  Post,
+  UsePipes,
+  ValidationPipe
+} from '@nestjs/common'
+import { Auth } from 'src/auth/decorators/auth.decorator'
 
-@Controller('order')
+import { OrderDto } from './dto/order.dto'
+import { PaymentStatusDto } from './dto/payment-status.dto'
+import { OrderService } from './order.service'
+import { CurrentUser } from 'src/user/decorator/user.decorator'
+
+@Controller('orders')
 export class OrderController {
-  constructor(private readonly orderService: OrderService) {}
+  constructor(private readonly orderService: OrderService) { }
+//создание платежа
+  @UsePipes(new ValidationPipe())
+  @HttpCode(200)
+  @Post('place')
+  @Auth()
+  async checkout(@Body() dto: OrderDto, @CurrentUser('id') userId: string) {
+    return this.orderService.createPayment(dto, userId)
+  }
+//изменение статуса
+  @HttpCode(200)
+  @Post('status')
+  async updateStatus(@Body() dto: PaymentStatusDto) {
+    return this.orderService.updateStatus(dto)
+  }
 }
